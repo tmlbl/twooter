2fe8c221b661e1c09a9b5361f3aa20c8d57294bf
= <tim.lebel@gmail.com>
2014-02-27 15:14:31 -0800

    Tweets are now jumbly

--- public/js/main.js
+++ public/js/main.js
@@ -12,12 +12,19 @@ var maker = _.template(template);
 
 $('#submit').click(function () {
   var hand = $('#handle').val();
+  if (!hand) {
+    alert('Please enter a twitter handle.');
+  }
   io.emit('handle', { handle: hand });
 });
 
 io.on('twoots', function (twoots) {
   $('#twoots').html(' ');
-  twats = twoots;
+  twats = jumble(twoots);
+  console.log(twats);
+  twats = _.map(twats, function (tweet) {
+    return { text: tweet, date: 'Just Now' };
+  });
   for (var tw in twats) {
     $('#twoots').append(maker(twats[tw]));
   }
@@ -25,6 +32,29 @@ io.on('twoots', function (twoots) {
 
 $("#handle").keyup(function(event){
     if(event.keyCode == 13){
-        $("#submit").click();
+      $("#submit").click();
     }
 });
+
+function jumble (tweets) {
+  var words = [],
+    newTweets = [];    
+  _.map(tweets, function (tweet) {
+    words = _.union(words, (tweet.text).split(' '));
+  });
+  nextWord = 5;
+  for (var i = 0; i <= 20; i++) {
+    var newTweet = '';
+    var tweetLen = Math.floor(Math.random()*140);
+    while (newTweet.length < tweetLen) {
+      var index = Math.floor(Math.random()*words.length);
+      newTweet += words[index] + ' ';
+      while (words[index].length < 5) {
+        index++;
+        newTweet += words[index] + ' ';
+      }
+    }
+    newTweets.push(newTweet);
+  }
+  return newTweets;
+}
